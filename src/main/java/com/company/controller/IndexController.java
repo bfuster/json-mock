147a80704b69c952f0082d7f44ac218ee1d4c267
Bruno Fuster <brunofuster@gmail.com>
2012-02-23 19:31:20 -0200

    first

--- src/main/java/com/company/controller/IndexController.java
+++ src/main/java/com/company/controller/IndexController.java
@@ -1,7 +1,6 @@
 package com.company.controller;
 
 import br.com.caelum.vraptor.Get;
-import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
